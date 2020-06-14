8f88f559928e58fb02417af19b9241c2ae947eb0
Steve Chan <zhiying.chen@formal-tech.com>
2020-06-14 15:19:10 +0800

    first commit

---
+++ 01-asciijson/AsciiJSON.go
@@ -0,0 +1,21 @@
+package asciijson
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/someJSON", func(c *gin.Context) {
+		data := map[string]interface{} {
+			"lang": "GO语言",
+			"tag": "<br>",
+		}
+
+		c.AsciiJSON(http.StatusOK, data)
+	})
+
+	r.Run(":8080")
+}
