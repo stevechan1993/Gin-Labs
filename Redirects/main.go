0ad922e8160b18fa3184ab22d97e9c03a3be8afc
Steve Chan <steve.d.chan@qq.com>
2020-06-26 11:57:50 +0800

    add redirects

---
+++ Redirects/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	r := gin.Default()
+
+	//r.GET("/test", func(c *gin.Context){
+	//	c.Redirect(http.StatusMovedPermanently, "http://www.google.com")
+	//})
+
+	r.POST("/test", func(c *gin.Context){
+		c.Redirect(http.StatusFound, "/foo")
+	})
+
+	r.GET("/test", func(c *gin.Context){
+		c.Request.URL.Path = "/test2"
+		r.HandleContext(c)
+	})
+
+	r.GET("/test2", func(c *gin.Context){
+		c.JSON(200, gin.H{"hello": "world"})
+	})
+
+	r.Run(":8080")
+}
